f8e734c19baa8bf3bafa4fe0caa6f23382bd8ee8
Keita Miyamura <kmiyamur0615@gmail.com>
2017-10-30 19:13:42 -0500

    udpated

--- weather.js
+++ weather.js
@@ -51,13 +51,13 @@ let handlePosition = function(info) {
   console.info("Latitude: " + latitude);
   console.info("Longitude: " + longitude);
 
-  let openweathermap_api_url = 'https://api.openweathermap.org/data/2.5/weather?'
-  openweathermap_api_url += 'lat=' + latitude
-  openweathermap_api_url += '&lon=' + longitude
-  openweathermap_api_url +='&appid=4ce6f502d38ddae567bf1702b05e168c&units=imperial'
+  let openweathermap_api_url = 'https://api.openweathermap.org/data/2.5/weather?';
+  openweathermap_api_url += 'lat=' + latitude;
+  openweathermap_api_url += '&lon=' + longitude;
+  openweathermap_api_url +='&appid=4ce6f502d38ddae567bf1702b05e168c&units=imperial';
 
   fetch(openweathermap_api_url).then(convertToJSON).then(updateWeather).catch(displayError);
-};
+}
 
 
 
